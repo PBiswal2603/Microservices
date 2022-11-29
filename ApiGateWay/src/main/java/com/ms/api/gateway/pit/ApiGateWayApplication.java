4b7268c0f12146da777bcb9cd327d4679b57aae8
biswa <biswalhere@gmail.com>
2022-11-29 23:17:05 +0530

    commiting changes

--- ApiGateWay/src/main/java/com/ms/api/gateway/pit/ApiGateWayApplication.java
+++ ApiGateWay/src/main/java/com/ms/api/gateway/pit/ApiGateWayApplication.java
@@ -3,6 +3,7 @@ package com.ms.api.gateway.pit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Main Spring Boot Class
 @SpringBootApplication
 public class ApiGateWayApplication {
 
